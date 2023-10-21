c5b686f6f027aed598d4df7ba6bb4bb2b6e4f9e5
lianshufeng <251708339@qq.com>
2023-10-22 00:25:59 +0800

    update

--- 更新到本地仓库.bat
+++ 更新到本地仓库.bat
@@ -2,20 +2,20 @@
 cd /d %~dp0
 
 echo base-java
-powershell -c "(Get-Content 'base-java\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jits:base' | Set-Content 'base-java\Dockerfile'"
+powershell -c "(Get-Content 'base-java\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base' | Set-Content 'base-java\Dockerfile'"
 
 
 echo prosody
-powershell -c "(Get-Content 'prosody\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jits:base' | Set-Content 'prosody\Dockerfile'"
+powershell -c "(Get-Content 'prosody\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base' | Set-Content 'prosody\Dockerfile'"
 
 
 echo web
-powershell -c "(Get-Content 'web\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jits:base' | Set-Content 'web\Dockerfile'"
+powershell -c "(Get-Content 'web\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base' | Set-Content 'web\Dockerfile'"
 
 echo jicofo
-powershell -c "(Get-Content 'jicofo\Dockerfile') -replace 'FROM \${JITSI_REPO}/base-java:\${BASE_TAG}', 'FROM lianshufeng/meet-jits:base-java' | Set-Content 'jicofo\Dockerfile'"
+powershell -c "(Get-Content 'jicofo\Dockerfile') -replace 'FROM \${JITSI_REPO}/base-java:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base-java' | Set-Content 'jicofo\Dockerfile'"
 
 echo jvb
-powershell -c "(Get-Content 'jvb\Dockerfile') -replace 'FROM \${JITSI_REPO}/base-java:\${BASE_TAG}', 'FROM lianshufeng/meet-jits:base-java' | Set-Content 'jvb\Dockerfile'"
+powershell -c "(Get-Content 'jvb\Dockerfile') -replace 'FROM \${JITSI_REPO}/base-java:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base-java' | Set-Content 'jvb\Dockerfile'"
 
 pause 
