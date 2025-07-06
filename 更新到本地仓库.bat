6b5d7368a814702c462f9f9bc13139f5637b7f5d
lianshufeng <251708339@qq.com>
2025-07-06 17:40:56 +0800

    更新

---
+++ 更新到本地仓库.bat
@@ -0,0 +1,32 @@
+@echo off
+cd /d %~dp0
+
+echo base-java
+powershell -c "(Get-Content 'base-java\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base' | Set-Content 'base-java\Dockerfile'"
+
+
+echo prosody
+powershell -c "(Get-Content 'prosody\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base' | Set-Content 'prosody\Dockerfile'"
+
+
+echo web
+powershell -c "(Get-Content 'web\Dockerfile') -replace 'FROM \${JITSI_REPO}/base:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base' | Set-Content 'web\Dockerfile'"
+
+echo jicofo
+powershell -c "(Get-Content 'jicofo\Dockerfile') -replace 'FROM \${JITSI_REPO}/base-java:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base-java' | Set-Content 'jicofo\Dockerfile'"
+
+echo jvb
+powershell -c "(Get-Content 'jvb\Dockerfile') -replace 'FROM \${JITSI_REPO}/base-java:\${BASE_TAG}', 'FROM lianshufeng/meet-jitsi:base-java' | Set-Content 'jvb\Dockerfile'"
+
+
+echo docker-compose.yml
+
+powershell -c "(Get-Content 'docker-compose.yml') -replace 'image: jitsi/web:\${JITSI_IMAGE_VERSION:-unstable}', 'image: lianshufeng/meet-jitsi:web' | Set-Content 'docker-compose.yml'"
+
+powershell -c "(Get-Content 'docker-compose.yml') -replace 'image: jitsi/prosody:\${JITSI_IMAGE_VERSION:-unstable}', 'image: lianshufeng/meet-jitsi:prosody' | Set-Content 'docker-compose.yml'"
+
+powershell -c "(Get-Content 'docker-compose.yml') -replace 'image: jitsi/jicofo:\${JITSI_IMAGE_VERSION:-unstable}', 'image: lianshufeng/meet-jitsi:jicofo' | Set-Content 'docker-compose.yml'"
+
+powershell -c "(Get-Content 'docker-compose.yml') -replace 'image: jitsi/jvb:\${JITSI_IMAGE_VERSION:-unstable}', 'image: lianshufeng/meet-jitsi:jvb' | Set-Content 'docker-compose.yml'"
+
+pause 
